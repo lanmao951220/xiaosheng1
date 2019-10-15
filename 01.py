b9d535e57667721899402ab9e46b58b8c04d72b8
Your Name <5679923@qq.com>
2019-10-15 23:16:48 +0800

    Initial commit

---
+++ 01.py
@@ -0,0 +1,12 @@
+class Student():
+    def __init__(self,name = "NoName",age=18):
+        self.name = age
+        self.age = age
+
+    def say(self):
+        print("My name is {0}".format(self.name))
+
+def sayHello():
+    print("Hi,欢迎来到图灵学院")
+
+print("我是模块p01，你特么叫我干毛")
---
+++ pkg02/p01.py
@@ -0,0 +1,13 @@
+class Student():
+    def __init__(self,name = "NoName",age=18):
+        self.name = age
+        self.age = age
+
+    def say(self):
+        print("My name is {0}".format(self.name))
+
+def sayHello():
+    print("Hi,欢迎来到图灵学院")
+# 此判断语句建议一直作为程序入口
+if __name__ == "__main__":
+    print("我是模块p01，你特么叫我干毛")
